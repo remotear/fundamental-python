999323fb08dca60da69a3fc15d2d4d5c34f98579
RJP <koekudukuad@gmail.com>
2020-06-23 01:15:05 +0700

    added new file

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+try:
+    r = requests.get ('https://google.com')
+    print(r.status_code)
+    if r.status_code == 200:
+        print(r.text)
+except Exception as e:
+    print('Ada error', e)
